f00dcd220a97babce45d4afda3349ac2ad0ad411
liuxiang <liuxiang@163.com>
2023-04-04 11:19:47 +0800

    no message

--- src/router/index.js
+++ src/router/index.js
@@ -6,29 +6,6 @@ Vue.use(Router);
 /* Layout */
 import Layout from "@/layout";
 
-/* Router Modules */
-
-/**
- * Note: sub-menu only appear when route children.length >= 1
- * Detail see: https://panjiachen.github.io/vue-element-admin-site/guide/essentials/router-and-nav.html
- *
- * hidden: true                   if set true, item will not show in the sidebar(default is false)
- * alwaysShow: true               if set true, will always show the root menu
- *                                if not set alwaysShow, when item has more than one children route,
- *                                it will becomes nested mode, otherwise not show the root menu
- * redirect: noRedirect           if set noRedirect will no redirect in the breadcrumb
- * name:'router-name'             the name is used by <keep-alive> (must set!!!)
- * meta : {
-    roles: ['admin','editor']    control the page roles (you can set multiple roles)
-    title: 'title'               the name show in sidebar and breadcrumb (recommend set)
-    icon: 'svg-name'/'el-icon-x' the icon show in the sidebar
-    noCache: true                if set true, the page will no be cached(default is false)
-    affix: true                  if set true, the tag will affix in the tags-view
-    breadcrumb: false            if set false, the item will hidden in breadcrumb(default is true)
-    activeMenu: '/example/list'  if set path, the sidebar will highlight the path you set
-  }
- */
-
 /**
  * constantRoutes
  * a base page that does not have permission requirements
@@ -315,47 +292,6 @@ export const asyncRoutes = [
       },
     ],
   },
-
-  /** when your routing map is too long, you can split it into small modules **/
-  // componentsRouter,
-
-  // {
-  //   path: "/tab",
-  //   component: Layout,
-  //   children: [
-  //     {
-  //       path: "index",
-  //       component: () => import("@/views/tab/index"),
-  //       name: "Tab",
-  //       meta: { title: "Tab", icon: "tab" },
-  //     },
-  //   ],
-  // },
-
-  // {
-  //   path: "/error",
-  //   component: Layout,
-  //   redirect: "noRedirect",
-  //   name: "ErrorPages",
-  //   meta: {
-  //     title: "Error Pages",
-  //     icon: "404",
-  //   },
-  //   children: [
-  //     {
-  //       path: "401",
-  //       component: () => import("@/views/error-page/401"),
-  //       name: "Page401",
-  //       meta: { title: "401", noCache: true },
-  //     },
-  //     {
-  //       path: "404",
-  //       component: () => import("@/views/error-page/404"),
-  //       name: "Page404",
-  //       meta: { title: "404", noCache: true },
-  //     },
-  //   ],
-  // },
 
   // 404 page must be placed at the end !!!
   { path: "*", redirect: "/404", hidden: true },
